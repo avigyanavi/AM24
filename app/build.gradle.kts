ef555175c750265764924dc1658c44395a7e7957
am24_avigyan <avigyan7@gmail.com>
2025-07-07 17:54:24 +0530

    Done - I think.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,7 +14,7 @@ android {
         applicationId = "com.am24.am24"
         minSdk = 26
         targetSdk = 35
-        versionCode = 57
+        versionCode = 58
         versionName = "0.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
