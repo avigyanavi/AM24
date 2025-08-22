5b5ab336df42fd0e4071f4a17c54fbb66bfe7a90
am24_avigyan <avigyan7@gmail.com>
2025-08-22 18:27:44 +0530

    Done.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -156,7 +156,6 @@ dependencies {
     implementation(libs.androidx.media3.exoplayer)
     implementation(libs.androidx.media3.ui)
     implementation(libs.androidx.work.runtime.ktx)
-    implementation("com.facebook.android:audience-network-sdk:6.20.0")
     implementation(libs.play.services.cast.tv)
     implementation(libs.firebase.auth)
     implementation(libs.mediation.test.suite)
