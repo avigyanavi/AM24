fb7bed870934f6a83979c8188c5c7e53bd040222
am24_avigyan <avigyan7@gmail.com>
2025-10-11 11:06:41 +0530

    Done.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,7 +15,7 @@ android {
         applicationId = "com.am24.am24"
         minSdk = 26
         targetSdk = 35
-        versionCode = 225
+        versionCode = 226
         versionName = "5.0"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
